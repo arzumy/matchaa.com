c84ebb78815b6eb100c331ec7dafaff8280608f7
Arzumy MD <hello@ar.zu.my>
2011-07-30 20:12:37 +0800

    basic user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,13 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :registerable, :token_authenticatable, :trackable, :validatable
 
+  validates_presence_of :name
+
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :name
+  attr_accessor :password
+
+  def password_required?
+    false
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 MatchaaCom::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "registrations" }
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
