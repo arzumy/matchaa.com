e9e0e68ab7cdc43eb9edbf0e3a0baf2bf01e324d
Arzumy MD <hello@ar.zu.my>
2011-07-31 01:38:40 +0800

    seeds for category

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+Category.find_or_create_by_name('The Real Matchaa')
+Category.find_or_create_by_name('Tech Mastaa')
+Category.find_or_create_by_name('Matchaa Love')
+
