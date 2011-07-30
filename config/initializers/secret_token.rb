b16d78e56ee556d1448aac24a26196177f4cf76c
Arzumy MD <hello@ar.zu.my>
2011-07-30 18:24:56 +0800

    bare rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MatchaaCom::Application.config.secret_token = '75e01edd472f929c54b8c5192bf1f290330bcf7774d0e4f81c645254f46ee615c8f013a00efcf250dc68126c7b39cb306fee12b57ad17411429bb752556683ae'
