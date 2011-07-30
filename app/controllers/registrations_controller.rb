502ff6b02e6e6d118f9a2aa08636fa1770595525
Arzumy MD <hello@ar.zu.my>
2011-07-31 04:18:24 +0800

    redirect user to create match after signup

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,8 @@
 class RegistrationsController < Devise::RegistrationsController
   def clean_up_passwords(resource)
   end
+
+  def redirect_location(resource_name, resource)
+    new_match_path
+  end
 end
