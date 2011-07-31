52d1a8997ff42edbb6d89e2cbb25573edc41a558
Arzumy MD <hello@ar.zu.my>
2011-07-31 10:21:57 +0800

    link up banner to matches/show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def banner(match)
-    render :partial => 'shared/banner', locals: { player1: match.player1, player2: match.player2 }
+    render :partial => 'shared/banner', locals: { match: match }
   end
 end
